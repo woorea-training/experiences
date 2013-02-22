1b8c6c3741f0e572d8de4d411cdbd4037d7c40e9
JAvvav19 <favea@orange.es>
2013-02-22 10:10:29 +0100

    Ultima Actualizacion 2

--- src/es/experiences/HelloWorld.java
+++ src/es/experiences/HelloWorld.java
@@ -6,7 +6,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Ultima Actualizacion");
+		System.out.println("Ultima Actualizacion 2");
 	}
 
 }
