e1123d0b4e2868dd3e65d2760b146f3270b58a1a
guillergoal <guillergoal@gmail.com>
2013-02-22 19:44:07 +0100

    A ver si me sube este ...

--- HowShit.java
+++ HowShit.java
@@ -3,10 +3,12 @@ public class HowShit {
 
 	/**
 	 * Los objetos de esta clase, seran cacas, como ella
+	 * y desde el eclipse, pues seguiran siendo una caca
 	 */
 	public static void main(String[] args) {
 
            System.out.println("Soy una caca de clase");
+           System.out.println("Del eclipse vengo ... pero total ...");
 		
 	}
 
