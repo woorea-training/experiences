a56410a5b4fce44e8e2170d406512da64d0165b1
guillergoal <guillergoal@gmail.com>
2013-02-22 18:29:21 +0100

    Pruebas con esto del git ...
    Pruebas con esto del git pero con un texto mas largo ...

---
+++ GunHead.java
@@ -0,0 +1,27 @@
+
+public class GunHead {
+
+	public static void main(String[] args) {
+
+/**
+ * CAUTION : remember this program 
+ * is an ONLY_ONE_EXECUTION_BY_PERSON_BEFORE_GO_HELL_TYPE (OOEBPBGTHT) :
+ * choose carefuly your last wishes ...
+ *     		
+ */
+		   if (args.length==0){
+			  System.out.println("Me pego el tiro sin mas ... PUMMM"); 
+		   } else {
+			   
+			   System.out.println("Mis ultimas voluntades son : "); 
+			   for (int i = 0;i<args.length;i++){
+				   int cualvoluntad = i;
+				   cualvoluntad++;
+				   System.out.println(cualvoluntad + " : " + args[i]);
+			   }
+		       System.out.println("Y dicho esto, me pego el tiro ... PUMMM"); 
+		   }
+		
+	}
+
+}
---
+++ HowShit.java
@@ -0,0 +1,13 @@
+
+public class HowShit {
+
+	/**
+	 * Los objetos de esta clase, seran cacas, como ella
+	 */
+	public static void main(String[] args) {
+
+           System.out.println("Soy una caca de clase");
+		
+	}
+
+}
