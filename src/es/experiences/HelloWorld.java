1dd314b4aa94ae5cd36ee96cc36846f4a29d578a
guillergoal <guillergoal@gmail.com>
2013-02-25 18:37:59 +0100

    ... Creo que me estoy volviendo medio gilipollas ...

--- src/es/experiences/HelloWorld.java
+++ src/es/experiences/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("v4");
+		System.out.println("v4 modificada por Willy");
+		
 	}
 
 }
